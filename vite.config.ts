367fd36c2b511df9fc207b0ca602f4d223273cc7
Matheus Castiglioni <mahenrique94@users.noreply.github.com>
2024-12-16 10:42:58 -0300

    Removing additional slash

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/tst-react-19/",
+  base: "/tst-react-19",
   plugins: [react()],
 });
